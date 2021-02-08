0e5e3c7a1b8c628db6e14d06f774213b68670129
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-02-08 19:04:37 -0400

    Initial Commit

---
+++ TimerApp_AppleWatch WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  TimerApp_AppleWatch WatchKit Extension
+//
+//  Created by Gustavo Anjos on 08/02/21.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ TimerApp_AppleWatch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  TimerApp_AppleWatch WatchKit Extension
+//
+//  Created by Gustavo Anjos on 08/02/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
